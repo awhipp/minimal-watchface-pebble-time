644e6dc1e85a5ac84bd73fb2cce529a620077e44
Alexander Whipp <awhipp@terpmail.umd.edu>
2015-12-31 20:08:09 -0500

    Base design - Issue with overlapping text layer

--- src/main.c
+++ src/main.c
@@ -1,24 +1,21 @@
 #include <pebble.h>
 
 static Window *s_main_window;
-static TextLayer *s_time_layer;
-
-static void update_time() {
-  // Get a tm structure
-  time_t temp = time(NULL); 
-  struct tm *tick_time = localtime(&temp);
-
-  // Write the current hours and minutes into a buffer
-  static char s_buffer[8];
-  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-                                          "%H:%M" : "%I:%M", tick_time);
-
-  // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, s_buffer);
-}
+static TextLayer *s_time_layer_hour;
+static TextLayer *s_time_layer_minute;
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
+  // Write the current hours and minutes into a buffer
+  static char s_buffer_hour[] = "00";
+  static char s_buffer_min[] = "00";
+  text_layer_set_text(s_time_layer_hour, "00");
+  text_layer_set_text(s_time_layer_minute, "00");
+
+  // Display this time on the TextLayer
+  strftime(s_buffer_hour, sizeof(s_buffer_hour), clock_is_24h_style() ? "%H" : "%I", tick_time);
+  text_layer_set_text(s_time_layer_hour, s_buffer_hour);
+  strftime(s_buffer_min, sizeof(s_buffer_min), clock_is_24h_style() ? "%M" : "%M", tick_time);
+  text_layer_set_text(s_time_layer_minute, s_buffer_min);
 }
 
 static void main_window_load(Window *window) {
@@ -27,23 +24,33 @@ static void main_window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   // Create the TextLayer with specific bounds
-  s_time_layer = text_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+  s_time_layer_hour = text_layer_create(
+      GRect(0, PBL_IF_ROUND_ELSE(29, 26), bounds.size.w, 50));
+  s_time_layer_minute = text_layer_create(
+      GRect(0, PBL_IF_ROUND_ELSE(87, 78), bounds.size.w, 50));
 
   // Improve the layout to be more like a watchface
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "00:00");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  text_layer_set_background_color(s_time_layer_hour, GColorClear);
+  text_layer_set_text_color(s_time_layer_hour, GColorRed);
+  text_layer_set_background_color(s_time_layer_minute, GColorClear);
+  text_layer_set_text_color(s_time_layer_minute, GColorBlue);
+  text_layer_set_text(s_time_layer_hour, "00:00");
+  text_layer_set_font(s_time_layer_hour, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
+  text_layer_set_text_alignment(s_time_layer_hour, GTextAlignmentCenter);
+  text_layer_set_text(s_time_layer_minute, "00:00");
+  text_layer_set_font(s_time_layer_minute, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
+  text_layer_set_text_alignment(s_time_layer_minute, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer_hour));
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer_minute));
 }
 
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
-  text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_time_layer_hour);
+  text_layer_destroy(s_time_layer_minute);
+  tick_timer_service_unsubscribe();
 }
 
 
@@ -51,6 +58,7 @@ static void init() {
   // Create main Window element and assign to pointer
   s_main_window = window_create();
 
+  window_set_background_color(s_main_window, GColorClear);
   // Set handlers to manage the elements inside the Window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
@@ -59,9 +67,12 @@ static void init() {
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
-
-  // Make sure the time is displayed from the start
-  update_time();
+  
+  // Ensures time is displayed immediately (will break if NULL tick event accessed).
+  // (This is why it's a good idea to have a separate routine to do the update itself.)
+  time_t now = time(NULL);
+  struct tm *current_time = localtime(&now);
+  tick_handler(current_time, MINUTE_UNIT);
 
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
