2ef73093a1c550dbd35d40029a911563e6617719
Alexander Whipp <awhipp@terpmail.umd.edu>
2016-01-01 21:45:16 -0500

    code for setting config

---
+++ src/pebble-js-app.js
@@ -0,0 +1,29 @@
+Pebble.addEventListener('ready', function() {
+  console.log('PebbleKit JS ready!');
+});
+
+Pebble.addEventListener('showConfiguration', function() {
+  var url = 'http://awhipp.github.io/patriotic-watchface-pebble-time/';
+  console.log('Showing configuration page: ' + url);
+
+  Pebble.openURL(url);
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  var configData = JSON.parse(decodeURIComponent(e.response));
+  console.log('Configuration page returned: ' + JSON.stringify(configData));
+
+  var backgroundColor = configData['country'];
+
+  var dict = {};
+  if(configData['country'] === true) {
+    dict['country'] = configData['country'];  
+  }
+
+  // Send to watchapp
+  Pebble.sendAppMessage(dict, function() {
+    console.log('Send successful: ' + JSON.stringify(dict));
+  }, function() {
+    console.log('Send failed!');
+  });
+});
